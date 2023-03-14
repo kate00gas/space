e0bed1e7c492472ed480033da689c64e68e7797b
kate00gas <115355854+kate00gas@users.noreply.github.com>
2023-03-14 20:09:16 +0300

    Add files via upload

---
+++ LR1.sql
@@ -0,0 +1,10 @@
+SET search_path TO lr_schema, public;
+SHOW search_path;
+
+--CREATE TABLE customers (
+	--no_cust     int          PRIMARY KEY,
+	--FIO         varchar(30),
+	--adress      varchar(30),
+	--ph_numb     int 
+--);
+
